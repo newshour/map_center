1ebacaf8d2b55a5ec0d682b0973436c37cf0e403
Vanessa Dennis <vdennis@gmail.com>
2012-07-12 15:37:25 -0400

    target blank on embed and nav change

--- embed/embed.php
+++ embed/embed.php
@@ -330,7 +330,7 @@ if ($map_module == 'static_maps' && $static_maps_type == '08general' && $map_vie
             <div id="main" role="main">
                 <div id="content">
                     <div id="emheader" class="cf">
-    	                <div id="emlogo"><a href="http://www.pbs.org/newshour/vote2012/map/"><img src="logo.png"></a></div>
+    	                <div id="emlogo"><a href="http://www.pbs.org/newshour/vote2012/map/" target="_blank"><img src="logo.png"></a></div>
                     </div>
                     <div id="view_info">
                         <h1><?php echo $map_title; ?></h1>
