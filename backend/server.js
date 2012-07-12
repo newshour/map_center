2879065673ed05b239043fc7230cd491bb1696da
Mike Pennisi <mike@mikepennisi.com>
2012-07-12 14:04:29 -0400

    Immediately send current map state to new clients

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 var portNumber = 8000,
     app = require("http").createServer(),
     io = require("socket.io").listen(app),
-    fs = require("fs");
+    fs = require("fs"),
+    currentMapState;
 
 app.listen(portNumber);
 
 io.sockets.on("connection", function (socket) {
+
+    // If a client connects after the map's state has been changed at least
+    // once, immediately send that state to the client
+    if (currentMapState) {
+        socket.emit("changeVotes", currentMapState);
+    }
+
     socket.on("changeVotes", function (data) {
+        currentMapState = data;
         io.sockets.emit("changeVotes", data);
-        console.log(data);
     });
 });
