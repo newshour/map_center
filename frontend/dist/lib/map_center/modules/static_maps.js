newshour/map_center

commit acfa01ce7f12dd8a7fc00f93286b19079464011a

Static maps tooltip fixes

frontend/dist/lib/map_center/modules/static_maps.js
@@ -196,7 +196,9 @@ $(document).one('coreInitialized', function() {
                 }
             }
         } else if (currentData.breaks) {
-            if (typeof(currentData.areas[thisFIPS]) == 'undefined' && typeof(currentData.areas[thisState]) == 'undefined') {
+            if (thisFIPS != '' && typeof(currentData.areas[thisFIPS]) == 'undefined') {
+                tooltipText.push('Unavailable');
+            } else if (typeof(currentData.areas[thisState]) == 'undefined') {
                 tooltipText.push('Unavailable');
             } else if (thisCounty != '') {
                 tooltipText.push((currentData.prefix || '') + currentData.areas[thisFIPS].toFixed(currentData.decimalPlaces || 0) + (currentData.suffix || '') + '</p>');

commit 5f811faacb8173e8cb53a5f73693bf22284efea7

Fixed bug introduced in acfa01ce7f12dd8a7fc00f93286b19079464011a
Tooltips on static maps showed data value as unavailable when using the default tooltip renderer with a data set for which county-level data did exist but state-level data did not

frontend/dist/lib/map_center/modules/static_maps.js
@@ -198,14 +198,18 @@ $(document).one('coreInitialized', function() {
                 }
             }
         } else if (currentData.breaks) {
-            if (thisFIPS != '' && typeof(currentData.areas[thisFIPS]) == 'undefined') {
-                tooltipText.push('Unavailable');
-            } else if (typeof(currentData.areas[thisState]) == 'undefined') {
-                tooltipText.push('Unavailable');
-            } else if (thisCounty != '') {
-                tooltipText.push((currentData.prefix || '') + currentData.areas[thisFIPS].toFixed(currentData.decimalPlaces || 0) + (currentData.suffix || '') + '</p>');
+            if (thisFIPS != '') {
+                if (typeof(currentData.areas[thisFIPS]) == 'undefined') {
+                    tooltipText.push('Unavailable');
+                } else {
+                    tooltipText.push((currentData.prefix || '') + currentData.areas[thisFIPS].toFixed(currentData.decimalPlaces || 0) + (currentData.suffix || '') + '</p>');
+                }
             } else {
-                tooltipText.push((currentData.prefix || '') + currentData.areas[thisState].toFixed(currentData.decimalPlaces || 0) + (currentData.suffix || '') + '</p>');
+                if (typeof(currentData.areas[thisState]) == 'undefined') {
+                    tooltipText.push('Unavailable');
+                } else {
+                    tooltipText.push((currentData.prefix || '') + currentData.areas[thisState].toFixed(currentData.decimalPlaces || 0) + (currentData.suffix || '') + '</p>');
+                }
             }
         }
         return tooltipText.join('');