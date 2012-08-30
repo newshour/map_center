baa4c478f507341bae1336a946bf4e5826782feb
Mike Pennisi <mike@mikepennisi.com>
2012-08-30 13:25:47 -0400

    Introduce UI for token management

--- backend/www/scripts/modules/auth.js
+++ backend/www/scripts/modules/auth.js
@@ -28,8 +28,51 @@ define([
         }
     });
 
+    var Token = Backbone.Model.extend({
+        parse: function(attributes) {
+            this.id = attributes.val;
+            delete attributes.val;
+            return attributes;
+        }
+    });
+    var Tokens = Backbone.Collection.extend({
+        model: Token,
+        url: "/token"
+    });
+
+    var TokenListItem = Backbone.View.extend({
+        template: JST["backend/templates/token-list-item.html"],
+        events: {
+            "click .delete": "destroy"
+        },
+        render: function() {
+            this.$el.html( this.template( this.model.toJSON() ) );
+            return this;
+        },
+        destroy: function() {
+            this.model.destroy();
+            this.$el.remove();
+        }
+    });
+    var TokenList = Backbone.View.extend({
+        template: JST["backend/templates/token-list.html"],
+        initialize: function() {
+            this.collection.on("reset", this.render, this);
+        },
+        render: function() {
+            this.$el.html( this.template() );
+            this.collection.each(function(model) {
+                this.$(".token-list").append(new TokenListItem({ model: model }).render().el);
+            }, this);
+            return this;
+        }
+    });
+
     return {
+        Token: Token,
+        Tokens: Tokens,
         views: {
+            tokenList: TokenList,
             loginForm: LoginForm
         }
     };
