newshour/map_center

commit d28da704b98e9ad241904eec0f76ae1959f0a779

other_votes style changes

frontend/dist/lib/map_center/modules/other_votes.js
@@ -1455,7 +1455,7 @@ $(document).one('coreInitialized', function() {
                 firstName: '',
                 lastName: 'Republicans',
                 photo: false,
-                votePercent: config.balanceOfPowerStart.governor.republican + republicanStates.length,
+                votePercent: republicanStates.length,
                 voteTotal: formatThousands(republicanVotes, 0),
                 winner: false,
                 color: config.partyColors["GOP"]
@@ -1465,7 +1465,7 @@ $(document).one('coreInitialized', function() {
                 firstName: '',
                 lastName: 'Democrats',
                 photo: false,
-                votePercent: config.balanceOfPowerStart.governor.democratic + democraticStates.length,
+                votePercent: democraticStates.length,
                 voteTotal: formatThousands(democraticVotes, 0),
                 winner: false,
                 color: config.partyColors["Dem"]
@@ -1479,7 +1479,7 @@ $(document).one('coreInitialized', function() {
                 firstName: '',
                 lastName: 'Other',
                 photo: false,
-                votePercent: config.balanceOfPowerStart.governor.thirdParty + thirdPartyStates.length,
+                votePercent: thirdPartyStates.length,
                 voteTotal: formatThousands(thirdPartyVotes, 0),
                 winner: false,
                 color: config.partyColors['thirdParty']
@@ -1568,7 +1568,7 @@ $(document).one('coreInitialized', function() {
                 firstName: '',
                 lastName: 'Republicans',
                 photo: false,
-                votePercent: config.balanceOfPowerStart.senate.republican + republicanStates.length,
+                votePercent: republicanStates.length,
                 voteTotal: formatThousands(republicanVotes, 0),
                 winner: false,
                 color: config.partyColors["GOP"]
@@ -1578,7 +1578,7 @@ $(document).one('coreInitialized', function() {
                 firstName: '',
                 lastName: 'Democrats',
                 photo: false,
-                votePercent: config.balanceOfPowerStart.senate.democratic + democraticStates.length,
+                votePercent: democraticStates.length,
                 voteTotal: formatThousands(democraticVotes, 0),
                 winner: false,
                 color: config.partyColors["Dem"]
@@ -1588,7 +1588,7 @@ $(document).one('coreInitialized', function() {
                 firstName: '',
                 lastName: 'Other',
                 photo: false,
-                votePercent: config.balanceOfPowerStart.senate.thirdParty + thirdPartyStates.length,
+                votePercent: thirdPartyStates.length,
                 voteTotal: formatThousands(thirdPartyVotes, 0),
                 winner: false,
                 color: config.partyColors['thirdParty']
@@ -1695,10 +1695,30 @@ $(document).one('coreInitialized', function() {
         // Render the legend items.
         var renderLegendItem = function(itemObj) {
             // bigElem: Boolean
-            if (itemObj.bigElem) {
-                var $itemElem = $('#legend_templates .candidate_big').clone();
+            if (raceNumber == 'President') {
+                if (itemObj.bigElem) {
+                    var $itemElem = $('#legend_templates .national_presidential_candidate_big').clone();
+                } else {
+                    var $itemElem = $('#legend_templates .national_presidential_candidate_small').clone();
+                }
+            } else if (raceNumber == 'Governor') {
+                if (itemObj.bigElem) {
+                    var $itemElem = $('#legend_templates .national_candidate_big').clone();
+                } else {
+                    var $itemElem = $('#legend_templates .national_candidate_small').clone();
+                }
+            } else if (raceNumber == 'U.S. Senate') {
+                if (itemObj.bigElem) {
+                    var $itemElem = $('#legend_templates .national_candidate_big').clone();
+                } else {
+                    var $itemElem = $('#legend_templates .national_candidate_small').clone();
+                }
             } else {
-                var $itemElem = $('#legend_templates .candidate_small').clone();
+                if (itemObj.bigElem) {
+                    var $itemElem = $('#legend_templates .national_candidate_big').clone();
+                } else {
+                    var $itemElem = $('#legend_templates .national_candidate_small').clone();
+                }
             }
             $itemElem.appendTo('#legend_candidates');
             if (itemObj.isOther) {$itemElem.addClass('legend_candidate_other');}

frontend/dist/all_results-broadcast-v.html
@@ -173,21 +173,58 @@
                         <div id="legend_templates">
                             <!-- These templates are cloned as necessary into #legend_candidates, and then their contents are set for their specific candidates. Change these to affect the markup for all of them. -->
                             <!-- .candidate_big is for the top three candidates (or however many are set in lib/map_center/modules/live.js, line 19); .candidate_small is for the rest. -->
+                            <!-- Legend items for the national results maps use the .national_* versions, except for the presidential race, which uses the .national_presidential_* versions. -->
                             <div class="candidate_big">
                                 <div class="candidate_color">&nbsp;</div>
                                 <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
                                 <div class="candidate_name">
                                     <span class="candidate_name_last">Lastname</span>
                                 </div>
-                                <div class="candidate_votes" style="font-size:2.9em;">269</div>
-                                <div class="candidate_votes_label" style="font-size:.6em; margin-top:0px; line-height:.9em; margin-left:79px; color:#9F1C20;">Electoral Votes</div>
-
-                                    
-
-                                <div class="candidate_won" style="display: block;"><img src="lib/images/won.png" /></div>
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
                                 <div class="clear"></div>
                             </div>
                             <div class="candidate_small">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first">Firstname</span>
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <div class="candidate_votes"></div>
+                            </div>
+                            <div class="national_candidate_big">
+                                <div class="candidate_color">&nbsp;</div>
+                                <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
+                                <div class="candidate_name">
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <!-- .candidate_vote_count is unused; .candidate_votes shows the number of seats won. -->
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_vote_count_wrapper">seats won</div>
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="national_candidate_small">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first">Firstname</span>
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <div class="candidate_votes"></div>
+                            </div>
+                            <div class="national_presidential_candidate_big">
+                                <div class="candidate_color">&nbsp;</div>
+                                <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
+                                <div class="candidate_name">
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <!-- .candidate_vote_count is unused; .candidate_votes is the number of electoral votes won. -->
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_vote_count_wrapper">electoral votes</div>
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="national_presidential_candidate_small">
                                 <div class="candidate_color">&nbsp;</div>
                                 <div class="candidate_name">
                                     <span class="candidate_name_first">Firstname</span>

frontend/dist/all_results-broadcast.html
@@ -173,6 +173,7 @@
                         <div id="legend_templates">
                             <!-- These templates are cloned as necessary into #legend_candidates, and then their contents are set for their specific candidates. Change these to affect the markup for all of them. -->
                             <!-- .candidate_big is for the top three candidates (or however many are set in lib/map_center/modules/live.js, line 19); .candidate_small is for the rest. -->
+                            <!-- Legend items for the national results maps use the .national_* versions, except for the presidential race, which uses the .national_presidential_* versions. -->
                             <div class="candidate_big">
                                 <div class="candidate_color">&nbsp;</div>
                                 <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
@@ -184,6 +185,46 @@
                                 <div class="clear"></div>
                             </div>
                             <div class="candidate_small">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first">Firstname</span>
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <div class="candidate_votes"></div>
+                            </div>
+                            <div class="national_candidate_big">
+                                <div class="candidate_color">&nbsp;</div>
+                                <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
+                                <div class="candidate_name">
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <!-- .candidate_vote_count is unused; .candidate_votes shows the number of seats won. -->
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_vote_count_wrapper">seats won</div>
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="national_candidate_small">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first">Firstname</span>
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <div class="candidate_votes"></div>
+                            </div>
+                            <div class="national_presidential_candidate_big">
+                                <div class="candidate_color">&nbsp;</div>
+                                <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
+                                <div class="candidate_name">
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <!-- .candidate_vote_count is unused; .candidate_votes is the number of electoral votes won. -->
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_vote_count_wrapper">electoral votes</div>
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="national_presidential_candidate_small">
                                 <div class="candidate_color">&nbsp;</div>
                                 <div class="candidate_name">
                                     <span class="candidate_name_first">Firstname</span>

frontend/dist/all_results-mobile.html
@@ -145,6 +145,7 @@
                         <div id="legend_templates">
                             <!-- These templates are cloned as necessary into #legend_candidates, and then their contents are set for their specific candidates. Change these to affect the markup for all of them. -->
                             <!-- .candidate_big is for the top three candidates (or however many are set in lib/map_center/modules/live.js, line 20); .candidate_small is for the rest. -->
+                            <!-- Legend items for the national results maps use the .national_* versions, except for the presidential race, which uses the .national_presidential_* versions. -->
                             <div class="candidate_big">
                                 <div class="candidate_color">&nbsp;</div>
                                 <div class="candidate_name">
@@ -167,6 +168,54 @@
                                     <span class="candidate_name_last">Lastname</span>
                                 </div>
                                 <div class="candidate_vote_count"></div>
+                                <div class="candidate_votes"></div>
+                            </div>
+                            <div class="national_candidate_big">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first"></span>
+                                    <span class="candidate_name_last"></span>
+                                </div>
+                                <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
+                                
+                                <!-- .candidate_vote_count is unused; .candidate_votes shows the number of seats won. -->
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_vote_count_wrapper">seats won</div>
+
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="national_candidate_small">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first">Firstname</span>
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <div class="candidate_vote_count_wrapper"></div>
+                                <div class="candidate_votes"></div>
+                            </div>
+                            <div class="national_presidential_candidate_big">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first"></span>
+                                    <span class="candidate_name_last"></span>
+                                </div>
+                                <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
+                                
+                                <!-- .candidate_vote_count is unused; .candidate_votes is the number of electoral votes won. -->
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_vote_count_wrapper">electoral votes</div>
+
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="national_presidential_candidate_small">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first">Firstname</span>
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <div class="candidate_vote_count_wrapper"></div>
                                 <div class="candidate_votes"></div>
                             </div>
                         </div>

frontend/dist/all_results.html
@@ -166,6 +166,7 @@
                         <div id="legend_templates">
                             <!-- These templates are cloned as necessary into #legend_candidates, and then their contents are set for their specific candidates. Change these to affect the markup for all of them. -->
                             <!-- .candidate_big is for the top three candidates (or however many are set in lib/map_center/modules/live.js, line 20); .candidate_small is for the rest. -->
+                            <!-- Legend items for the national results maps use the .national_* versions, except for the presidential race, which uses the .national_presidential_* versions. -->
                             <div class="candidate_big">
                                 <div class="candidate_color">&nbsp;</div>
                                 <div class="candidate_name">
@@ -188,6 +189,54 @@
                                     <span class="candidate_name_last">Lastname</span>
                                 </div>
                                 <div class="candidate_vote_count"></div>
+                                <div class="candidate_votes"></div>
+                            </div>
+                            <div class="national_candidate_big">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first"></span>
+                                    <span class="candidate_name_last"></span>
+                                </div>
+                                <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
+                                
+                                <!-- .candidate_vote_count is unused; .candidate_votes shows the number of seats won. -->
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_vote_count_wrapper">seats won</div>
+
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="national_candidate_small">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first">Firstname</span>
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <div class="candidate_vote_count_wrapper"></div>
+                                <div class="candidate_votes"></div>
+                            </div>
+                            <div class="national_presidential_candidate_big">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first"></span>
+                                    <span class="candidate_name_last"></span>
+                                </div>
+                                <img class="candidate_image" src="candidate_placeholder_50x75.png" width="66" height="69" />
+                                
+                                <!-- .candidate_vote_count is unused; .candidate_votes is the number of electoral votes won. -->
+                                <div class="candidate_votes"></div>
+                                <div class="candidate_vote_count_wrapper">electoral votes</div>
+
+                                <div class="candidate_won"><img src="lib/images/won.png" /></div>
+                                <div class="clear"></div>
+                            </div>
+                            <div class="national_presidential_candidate_small">
+                                <div class="candidate_color">&nbsp;</div>
+                                <div class="candidate_name">
+                                    <span class="candidate_name_first">Firstname</span>
+                                    <span class="candidate_name_last">Lastname</span>
+                                </div>
+                                <div class="candidate_vote_count_wrapper"></div>
                                 <div class="candidate_votes"></div>
                             </div>
                         </div>

frontend/dist/lib/map_center/modules/iowa-broadcast.css
@@ -33,8 +33,8 @@
     }
 	
 	#sidebar_title span {color:9f1c20;}
-	.candidate_big {margin:0 0 7px 0 ; padding-left:30px;}
-	.candidate_big .candidate_name {
+	.candidate_big, .national_candidate_big, .national_presidential_candidate_big {margin:0 0 7px 0 ; padding-left:30px;}
+	.candidate_big .candidate_name, .national_candidate_big .candidate_name, .national_presidential_candidate_big .candidate_name {
 			text-shadow: 1px 1px 2px rgba(255, 255, 255, 1);
             font-size: 1.3em;
             font-weight: bold;
@@ -43,7 +43,7 @@
 			line-height:.8em;
 			
         }
-        .candidate_big .candidate_votes {
+        .candidate_big .candidate_votes, .national_candidate_big .candidate_votes, .national_presidential_candidate_big .candidate_votes {
 			text-shadow: 1px 1px 2px rgba(255, 255, 255, 1);
             margin-top: 3px;
             text-align: left;
@@ -52,6 +52,14 @@
 			font-weight:bold;
 			line-height:1em;
         }
+        .national_candidate_big .candidate_votes, .national_presidential_candidate_big .candidate_votes {font-size: 2.9em;}
+        .national_candidate_big .candidate_vote_count_wrapper, .national_presidential_candidate_big .candidate_vote_count_wrapper {
+            font-size:.6em;
+            margin-top: 0px;
+            line-height: .9em;
+            color: #9F1C20;
+        }
+        .national_presidential_candidate_big .candidate_vote_count_wrapper {margin-left: 79px;}
 		.candidate_image {margin-top:5px; margin-bottom:0px; margin-right:8px;}
 		    .candidate_color {
             height: 103%;

frontend/dist/lib/map_center/modules/live.css
@@ -60,11 +60,11 @@
 #legend_candidates {margin-bottom: 10px;}
 #legend_templates {display: none;}
     
-    .candidate_big, .candidate_small {
+    .candidate_big, .candidate_small, .national_candidate_big, .national_candidate_small, .national_presidential_candidate_big, .national_presidential_candidate_small {
         position: relative;
 		font-family:Arial, Helvetica, sans-serif;
     }
-    .candidate_big {
+    .candidate_big, .national_candidate_big, .national_presidential_candidate_big {
 		        padding: 0px 5px 5px 30px;
 
         background-color: #efefef;
@@ -81,7 +81,7 @@
 		url('../../images/bg-big.jpg');
 		background-repeat:repeat-x;
     }
-    .candidate_small {
+    .candidate_small, .national_candidate_small, .national_presidential_candidate_small {
 		        padding: 5px 5px 5px 10px;
 
         background-color: #d1d0d0;
@@ -105,7 +105,7 @@
             top: -1%;
             width: 25px;
         }
-		.candidate_small .candidate_color {width:15px; }
+		.candidate_small .candidate_color, .national_candidate_small .candidate_color, .national_presidential_candidate_small .candidate_color {width:15px; }
         .candidate_won {
             display: none;
             position: absolute;
@@ -113,7 +113,7 @@
             top: -25px;
         }
         
-        .candidate_big .candidate_name {
+        .candidate_big .candidate_name, .national_candidate_big .candidate_name, .national_presidential_candidate_big .candidate_name {
 			text-shadow: 1px 1px 1px rgba(255, 255, 255, 1);
             font-size: 1.4em;
             font-weight: bold;
@@ -121,7 +121,7 @@
 			line-height:1.1em;
 			margin:2px 0 2px 0;
         }
-        .candidate_big .candidate_votes {
+        .candidate_big .candidate_votes, .national_candidate_big .candidate_votes, .national_presidential_candidate_big .candidate_votes {
             text-align: left;
 			color:#9f1c20;
 			font-weight:bold;
@@ -130,19 +130,19 @@
 
 
         }
-		.candidate_big .vote_wrap { width:200px; overflow:visible; height:69px; }
-        .candidate_big .candidate_vote_count {
+		.candidate_big .vote_wrap, .national_candidate_big .vote_wrap, .national_presidential_candidate_big .vote_wrap { width:200px; overflow:visible; height:69px; }
+        .candidate_big .candidate_vote_count, .national_candidate_big .candidate_vote_count, .national_presidential_candidate_big .candidate_vote_count {
 						margin:0 0 0 2px;
 						
 				
 
 			
         }
-		.candidate_big .candidate_vote_count_wrapper {font-size:1em;}
+		.candidate_big .candidate_vote_count_wrapper, .national_candidate_big .candidate_vote_count_wrapper, .national_presidential_candidate_big .candidate_vote_count_wrapper {font-size:1em;}
         
-        .candidate_small .candidate_name {float: left;}
-        .candidate_small .candidate_votes {text-align: right; font-weight:bold;}
-        .candidate_small .candidate_vote_count {
+        .candidate_small .candidate_name, .national_candidate_small .candidate_name, .national_presidential_candidate_small .candidate_name {float: left;}
+        .candidate_small .candidate_votes, .national_candidate_small .candidate_votes, .national_presidential_candidate_small .candidate_votes {text-align: right; font-weight:bold;}
+        .candidate_small .candidate_vote_count, .national_candidate_small .candidate_vote_count, .national_presidential_candidate_small .candidate_vote_count {
             float: left;
             font-size: 0.85em;
             margin-top: 0.125em;

commit 86198cfd56334663a9360fac15a7a803baf3c9d4

Fixed regression where national presidential "Other" expansion failed due to class names introduced in d28da704b98e9ad241904eec0f76ae1959f0a779

frontend/dist/lib/map_center/modules/other_votes.js
@@ -1810,7 +1810,7 @@ $(document).one('coreInitialized', function() {
             // condensed some of the candidates into that entry.
             if (config.tooltipsEnabled && config.condenseCandidates && config.hoverExpandOther) {
                 // Find the "Other" legend entry.
-                var otherElement = $('.candidate_small').filter(function(i) {
+                var otherElement = $('.national_presidential_candidate_small').filter(function(i) {
                     var candidateName = $(this).find('.candidate_name');
                     if (candidateName.find('.candidate_name_last').text() == 'Other' && candidateName.find('.candidate_name_first').text() == '') {
                         return true;